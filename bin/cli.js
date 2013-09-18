fcdf4db42103c43e10342345c7ccaf136d7d388d
David Jensen <david@jensen47.com>
2013-09-17 18:02:02 -0700

    Fixed line indentation, from 4 to 2.
    Node.js JavaScript style guidelines call for 2 space indentation.
    http://nodeguide.com/style.html#tabs-vs-spaces

--- bin/cli.js
+++ bin/cli.js
@@ -1,67 +1,67 @@
 #!/usr/bin/env node
 
 var path = require('path'),
-    optimist = require('optimist'),
-    usage = require('./usage'),
-    commands = require('../lib/commands'),
-    properties = require('../lib/properties'),
-    version = require('../package.json').version;
+  optimist = require('optimist'),
+  usage = require('./usage'),
+  commands = require('../lib/commands'),
+  properties = require('../lib/properties'),
+  version = require('../package.json').version;
 
 var die = function(msg) {
-    console.error('\n', msg, '\n');
-    console.info(usage);
+  console.error('\n', msg, '\n');
+  console.info(usage);
 
-    process.exit(1);
+  process.exit(1);
 }
 
 var argv = optimist
-    .usage(usage)
-    .argv;
+  .usage(usage)
+  .argv;
 
 if (argv.h || argv.help) {
-    console.info(usage);
-    return;
+  console.info(usage);
+  return;
 }
 
 if (argv.v || argv.version) {
-    console.info('v' + version);
-    return;
+  console.info('v' + version);
+  return;
 }
 
 if (argv._.length == 0) {
-    die('Command argument is required');
+  die('Command argument is required');
 }
 
 var commandName = argv._.shift().toLowerCase();
 
 if (!commands[commandName]) {
-    die('Command ' + commandName + ' is not known to truss!');
+  die('Command ' + commandName + ' is not known to truss!');
 }
 
 try {
-    var options = properties.parse(argv._);
+  var options = properties.parse(argv._);
 
-    options.templateDir = path.join(__dirname, '..', 'templates');
+  options.templateDir = path.join(__dirname, '..', 'templates');
 
-    if (process.env.TRUSS_TEMPLATE_DIR) {
-        options.templateDir = process.env.TRUSS_TEMPLATE_DIR;
+  if (process.env.TRUSS_TEMPLATE_DIR) {
+    options.templateDir = process.env.TRUSS_TEMPLATE_DIR;
+  }
+
+  if (argv.templateDir || argv.t) {
+    options.templateDir = argv.templateDir || argv.t;
+  }
+
+  // copy switches from argv to options.switches
+  options.switches = {};
+
+  for (var key in argv) {
+    if (key != '_' && key != '$0' && argv.hasOwnProperty(key)) {
+      options.switches[key] = argv[key];
     }
+  }
 
-    if (argv.templateDir || argv.t) {
-        options.templateDir = argv.templateDir || argv.t;
-    }
-
-    // copy switches from argv to options.switches
-    options.switches = {};
-    
-    for (var key in argv) {
-        if (key != '_' && key != '$0' && argv.hasOwnProperty(key)) {
-            options.switches[key] = argv[key];
-        }
-    }
-
-    commands[commandName](options);
+  commands[commandName](options);
 } catch (e) {
-    die(e);
+  die(e);
 }
 
